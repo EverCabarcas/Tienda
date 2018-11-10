d3bb199ab2c8f9c16072765e76fa73da306979b4
EverCabarcas <evercm95@hotmail.com>
2018-11-10 10:07:03 -0500

    limpieza de codigo

--- src/components/PrincipalPage.vue
+++ src/components/PrincipalPage.vue
@@ -51,22 +51,6 @@
         >
         <category v-if="vcategoria == 1"></category>
         <catalog  v-if="vcatalogo == 1"></catalog>
-          <!--
-          <v-flex text-xs-center>
-            <v-tooltip left>
-              <v-btn slot="activator" :href="source" icon large target="_blank">
-                <v-icon large>code</v-icon>
-              </v-btn>
-              <span>Source</span>
-            </v-tooltip>
-            <v-tooltip right>
-              <v-btn slot="activator" icon large href="https://codepen.io/johnjleider/pen/rJdVMq" target="_blank">
-                <v-icon large>mdi-codepen</v-icon>
-              </v-btn>
-              <span>Codepen</span>
-            </v-tooltip>
-          </v-flex>
--->
         </v-layout>
       </v-container>
     </v-content>
